36574178688ba6e9f07554f1007f2b3501b6b20b
Artem Kropotov <aakropotov@alfabank.ru>
2022-02-27 20:43:47 +0300

    lesson 10: Доработали конфиг для AuditorAware

--- src/main/java/ru/gb/config/ShopConfig.java
+++ src/main/java/ru/gb/config/ShopConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import ru.gb.dao.security.AccountUserDao;
+import ru.gb.entity.security.AccountUser;
 
 import java.util.Optional;
 
@@ -11,8 +16,15 @@ import java.util.Optional;
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareBean")
 public class ShopConfig {
 
+
+    //  если в полях createdBy и updateBy хотим испольльзовать не String username а полноценного пользователя то раскомментировать и использовать этот код
     @Bean
-    public AuditorAware<String> auditorAwareBean() {
-        return () -> Optional.of("User");
+//    public AuditorAware<AccountUser> auditorAwareBean(AccountUserDao accountUserDao) {
+    public AuditorAware<String> auditorAwareBean(AccountUserDao accountUserDao) {
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getName);
+//                .flatMap(accountUserDao::findByUsername);
     }
 }
